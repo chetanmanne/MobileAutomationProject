7a74e23e5133947ab1a57b2931c2f8da4b576574
chetanmanne <chetanmanne@gmail.com>
2017-04-14 12:41:56 -0500

    adding project

---
+++ testobject_shoppinglist_android-master/demo/src/main/java/com/testobject/demo/pageobjects/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all the necessary Page Objects for Shopping App
+ * 
+ * @author Chetan(chetanmanne@gmail.com)
+ */
+package com.testobject.demo.pageobjects;
---
+++ testobject_shoppinglist_android-master/demo/src/test/java/com/testobject/demo/testng/tests/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Shopping App Test Suites Execution using TestNG
+ * 
+ * @author Chetan(chetanmanne@gmail.com)
+ */
+package com.testobject.demo.testng.tests;
